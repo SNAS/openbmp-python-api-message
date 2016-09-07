68de38fd13ae7c39f2d1f924ad8a2cc71ba047af
omerpalaz <omer.palaz@gmail.com>
2016-09-07 14:08:01 -0700

    - LsLink class is compatible with java codebase.

--- src/openbmp/api/parsed/message/LsLink.py
+++ src/openbmp/api/parsed/message/LsLink.py
@@ -8,6 +8,7 @@
 from Base import *
 from FieldProcessors import *
 from Message import *
+from MsgBusFields import MsgBusFields
 
 class LsLink(Base):
     """
@@ -15,6 +16,15 @@ class LsLink(Base):
 
         Schema Version: 1.3
     """
+
+    minimumHeaderNames = [MsgBusFields.ACTION['name'],MsgBusFields.SEQUENCE['name'],MsgBusFields.HASH['name'],MsgBusFields.BASE_ATTR_HASH['name'],MsgBusFields.ROUTER_HASH['name'],
+                            MsgBusFields.ROUTER_IP['name'],MsgBusFields.PEER_HASH['name'],MsgBusFields.PEER_IP['name'],MsgBusFields.PEER_ASN['name'],MsgBusFields.TIMESTAMP['name'],
+                            MsgBusFields.IGP_ROUTER_ID['name'],MsgBusFields.ROUTER_ID['name'],MsgBusFields.ROUTING_ID['name'],MsgBusFields.LS_ID['name'],MsgBusFields.OSPF_AREA_ID['name'],
+                            MsgBusFields.ISIS_AREA_ID['name'],MsgBusFields.PROTOCOL['name'],MsgBusFields.AS_PATH['name'],MsgBusFields.LOCAL_PREF['name'],MsgBusFields.MED['name'],
+                            MsgBusFields.NEXTHOP['name'],MsgBusFields.MT_ID['name'],MsgBusFields.LOCAL_LINK_ID['name'],MsgBusFields.REMOTE_LINK_ID['name'],MsgBusFields.INTF_IP['name'],
+                            MsgBusFields.NEI_IP['name'],MsgBusFields.IGP_METRIC['name'],MsgBusFields.ADMIN_GROUP['name'],MsgBusFields.MAX_LINK_BW['name'],MsgBusFields.MAX_RESV_BW['name'],
+                            MsgBusFields.UNRESV_BW['name'],MsgBusFields.TE_DEFAULT_METRIC['name'],MsgBusFields.LINK_PROTECTION['name'],MsgBusFields.MPLS_PROTO_MASK['name'],
+                            MsgBusFields.SRLG['name'],MsgBusFields.LINK_NAME['name'],MsgBusFields.REMOTE_NODE_HASH['name'],MsgBusFields.LOCAL_NODE_HASH['name']]
 
     def __init__(self, message):
         """
@@ -32,37 +42,18 @@ class LsLink(Base):
         self.spec_version = version
 
         if version >= float(1.3):
-
-            self.headerNames = ["action", "seq", "hash", "base_attr_hash", "router_hash", "router_ip", "peer_hash", "peer_ip",
-                "peer_asn", "timestamp", "igp_router_id", "router_id", "routing_id", "ls_id",
-                "ospf_area_id", "isis_area_id", "protocol", "as_path", "local_pref", "med", "nexthop",
-                "mt_id", "local_link_id", "remote_link_id", "intf_ip", "nei_ip", "igp_metric",
-                "admin_group", "max_link_bw", "max_resv_bw", "unresv_bw", "te_default_metric",
-                "link_protection", "mpls_proto_mask", "srlg", "link_name", "remote_node_hash",
-                "local_node_hash", "remote_igp_router_id", "remote_router_id", "local_node_asn",
-                "remote_node_asn", "peer_node_sid", "isPrePolicy", "isAdjRibIn"]
+            versionSpecificHeaders = [MsgBusFields.REMOTE_IGP_ROUTER_ID.getName(),MsgBusFields.REMOTE_ROUTER_ID.getName(),MsgBusFields.LOCAL_NODE_ASN.getName(),MsgBusFields.REMOTE_NODE_ASN.getName(),
+                                      MsgBusFields.PEER_NODE_SID.getName(),MsgBusFields.ISPREPOLICY.getName(),MsgBusFields.IS_ADJ_RIB_IN.getName()]
 
         elif version >= float(1.2):
-
-            self.headerNames = ["action", "seq", "hash", "base_attr_hash", "router_hash", "router_ip", "peer_hash", "peer_ip",
-                "peer_asn", "timestamp", "igp_router_id", "router_id", "routing_id", "ls_id",
-                "ospf_area_id", "isis_area_id", "protocol", "as_path", "local_pref", "med", "nexthop",
-                "mt_id", "local_link_id", "remote_link_id", "intf_ip", "nei_ip", "igp_metric",
-                "admin_group", "max_link_bw", "max_resv_bw", "unresv_bw", "te_default_metric",
-                "link_protection", "mpls_proto_mask", "srlg", "link_name", "remote_node_hash",
-                "local_node_hash", "remote_igp_router_id", "remote_router_id", "local_node_asn",
-                "remote_node_asn", "peer_node_sid"]
+            versionSpecificHeaders = [MsgBusFields.REMOTE_IGP_ROUTER_ID.getName(),MsgBusFields.REMOTE_ROUTER_ID.getName(),MsgBusFields.LOCAL_NODE_ASN.getName(),
+                                      MsgBusFields.REMOTE_NODE_ASN.getName(),MsgBusFields.PEER_NODE_SID.getName()]
 
         else:
+            versionSpecificHeaders = []
 
-            self.headerNames = ["action", "seq", "hash", "base_attr_hash", "router_hash", "router_ip", "peer_hash", "peer_ip",
-                "peer_asn", "timestamp", "igp_router_id", "router_id", "routing_id", "ls_id",
-                "ospf_area_id", "isis_area_id", "protocol", "as_path", "local_pref", "med", "nexthop",
-                "mt_id", "local_link_id", "remote_link_id", "intf_ip", "nei_ip", "igp_metric",
-                "admin_group", "max_link_bw", "max_resv_bw", "unresv_bw", "te_default_metric",
-                "link_protection", "mpls_proto_mask", "srlg", "link_name", "remote_node_hash",
-                "local_node_hash"]
-
+        # Concatenate minimum header names and version specific header names.
+        self.headerNames = LsLink.minimumHeaderNames + versionSpecificHeaders
         self.parse(version, data)
 
     def getProcessors(self):
@@ -73,150 +64,74 @@ class LsLink(Base):
         :return: array of cell processors.
         """
 
-        processors = None
+        defaultCellProcessors = [
+
+            NotNull(),  # action
+            ParseLong(),  # seq
+            NotNull(),  # hash
+            NotNull(),  # base_hash
+            NotNull(),  # router_hash
+            NotNull(),  # router_ip
+            NotNull(),  # peer_hash
+            NotNull(),  # peer_ip
+            ParseLong(),  # peer_asn
+            ParseTimestamp(),  # timestamp
+            ParseNullAsEmpty(),  # igp_router_id
+            ParseNullAsEmpty(),  # router_id
+            ParseNullAsEmpty(),  # routing_id
+            ParseLongEmptyAsZero(),  # ls_id
+            ParseNullAsEmpty(),  # ospf_area_id
+            ParseNullAsEmpty(),  # isis_area_id
+            ParseNullAsEmpty(),  # protocol
+            ParseNullAsEmpty(),  # as_path
+            ParseLongEmptyAsZero(),  # local_pref
+            ParseLongEmptyAsZero(),  # med
+            ParseNullAsEmpty(),  # nexthop
+            ParseNullAsEmpty(),  # mt_id
+            ParseLongEmptyAsZero(),  # local_link_id
+            ParseLongEmptyAsZero(),  # remote_link_id
+            ParseNullAsEmpty(),  # intf_ip
+            ParseNullAsEmpty(),  # nei_ip
+            ParseLongEmptyAsZero(),  # igp_metric
+            ParseLongEmptyAsZero(),  # admin_group
+            ParseNullAsEmpty(),  # max_link_bw
+            ParseNullAsEmpty(),  # max_resv_bw
+            ParseNullAsEmpty(),  # unresv_bw
+            ParseLongEmptyAsZero(),  # te_default_metric
+            ParseNullAsEmpty(),  # link_protection
+            ParseNullAsEmpty(),  # mpls_proto_mask
+            ParseNullAsEmpty(),  # srlg
+            ParseNullAsEmpty(),  # link_name
+            ParseNullAsEmpty(),  # remote_node_hash
+            ParseNullAsEmpty(),  # local_node_hash
+        ]
 
         if self.spec_version >= float(1.3):
 
-            processors = [
+            versionSpecificProcessors = [
 
-                NotNull(), # action
-                ParseLong(), # seq
-                NotNull(), # hash
-                NotNull(), # base_hash
-                NotNull(), # router_hash
-                NotNull(), # router_ip
-                NotNull(), # peer_hash
-                NotNull(), # peer_ip
-                ParseLong(), # peer_asn
-                ParseTimestamp(), # timestamp
-                ParseNullAsEmpty(), # igp_router_id
-                ParseNullAsEmpty(), # router_id
-                ParseNullAsEmpty(), # routing_id
-                ParseLongEmptyAsZero(), # ls_id
-                ParseNullAsEmpty(), # ospf_area_id
-                ParseNullAsEmpty(), # isis_area_id
-                ParseNullAsEmpty(), # protocol
-                ParseNullAsEmpty(), # as_path
-                ParseLongEmptyAsZero(), # local_pref
-                ParseLongEmptyAsZero(), # med
-                ParseNullAsEmpty(), # nexthop
-                ParseNullAsEmpty(), # mt_id
-                ParseLongEmptyAsZero(), # local_link_id
-                ParseLongEmptyAsZero(), # remote_link_id
-                ParseNullAsEmpty(), # intf_ip
-                ParseNullAsEmpty(), # nei_ip
-                ParseLongEmptyAsZero(), # igp_metric
-                ParseLongEmptyAsZero(), # admin_group
-                ParseNullAsEmpty(), # max_link_bw
-                ParseNullAsEmpty(), # max_resv_bw
-                ParseNullAsEmpty(), # unresv_bw
-                ParseLongEmptyAsZero(), # te_default_metric
-                ParseNullAsEmpty(), # link_protection
-                ParseNullAsEmpty(), # mpls_proto_mask
-                ParseNullAsEmpty(), # srlg
-                ParseNullAsEmpty(), # link_name
-                ParseNullAsEmpty(), # remote_node_hash
-                ParseNullAsEmpty(), # local_node_hash
-                ParseNullAsEmpty(), # remote_igp_router_id
-                ParseNullAsEmpty(), # remote_router_id
-                ParseLongEmptyAsZero(), # local_node_asn
-                ParseLongEmptyAsZero(), # remote_node_asn
-                ParseNullAsEmpty(), # Peer node SID
-                ParseLongEmptyAsZero(), # isPrePolicy
-                ParseLongEmptyAsZero() # isAdjRibIn
+                ParseNullAsEmpty(),  # remote_igp_router_id
+                ParseNullAsEmpty(),  # remote_router_id
+                ParseLongEmptyAsZero(),  # local_node_asn
+                ParseLongEmptyAsZero(),  # remote_node_asn
+                ParseNullAsEmpty(),  # Peer node SID
+                ParseLongEmptyAsZero(),  # isPrePolicy
+                ParseLongEmptyAsZero()  # isAdjRibIn
             ]
 
         elif self.spec_version >= float(1.2):
 
-            processors = [
+            versionSpecificProcessors = [
 
-                NotNull(), # action
-                ParseLong(), # seq
-                NotNull(), # hash
-                NotNull(), # base_hash
-                NotNull(), # router_hash
-                NotNull(), # router_ip
-                NotNull(), # peer_hash
-                NotNull(), # peer_ip
-                ParseLong(), # peer_asn
-                ParseTimestamp(), # timestamp
-                ParseNullAsEmpty(), # igp_router_id
-                ParseNullAsEmpty(), # router_id
-                ParseNullAsEmpty(), # routing_id
-                ParseLongEmptyAsZero(), # ls_id
-                ParseNullAsEmpty(), # ospf_area_id
-                ParseNullAsEmpty(), # isis_area_id
-                ParseNullAsEmpty(), # protocol
-                ParseNullAsEmpty(), # as_path
-                ParseLongEmptyAsZero(), # local_pref
-                ParseLongEmptyAsZero(), # med
-                ParseNullAsEmpty(), # nexthop
-                ParseNullAsEmpty(), # mt_id
-                ParseLongEmptyAsZero(), # local_link_id
-                ParseLongEmptyAsZero(), # remote_link_id
-                ParseNullAsEmpty(), # intf_ip
-                ParseNullAsEmpty(), # nei_ip
-                ParseLongEmptyAsZero(), # igp_metric
-                ParseLongEmptyAsZero(), # admin_group
-                ParseNullAsEmpty(), # max_link_bw
-                ParseNullAsEmpty(), # max_resv_bw
-                ParseNullAsEmpty(), # unresv_bw
-                ParseLongEmptyAsZero(), # te_default_metric
-                ParseNullAsEmpty(), # link_protection
-                ParseNullAsEmpty(), # mpls_proto_mask
-                ParseNullAsEmpty(), # srlg
-                ParseNullAsEmpty(), # link_name
-                ParseNullAsEmpty(), # remote_node_hash
-                ParseNullAsEmpty(), # local_node_hash
-                ParseNullAsEmpty(), # remote_igp_router_id
-                ParseNullAsEmpty(), # remote_router_id
-                ParseLongEmptyAsZero(), # local_node_asn
-                ParseLongEmptyAsZero(), # remote_node_asn
-                ParseNullAsEmpty() # Peer node SID
+                ParseNullAsEmpty(),  # remote_igp_router_id
+                ParseNullAsEmpty(),  # remote_router_id
+                ParseLongEmptyAsZero(),  # local_node_asn
+                ParseLongEmptyAsZero(),  # remote_node_asn
+                ParseNullAsEmpty()  # Peer node SID
             ]
 
         else:
 
-            processors = [
+            versionSpecificProcessors = []
 
-                NotNull(), # action
-                ParseLong(), # seq
-                NotNull(), # hash
-                NotNull(), # base_hash
-                NotNull(), # router_hash
-                NotNull(), # router_ip
-                NotNull(), # peer_hash
-                NotNull(), # peer_ip
-                ParseLong(), # peer_asn
-                ParseTimestamp(), # timestamp
-                ParseNullAsEmpty(), # igp_router_id
-                ParseNullAsEmpty(), # router_id
-                ParseNullAsEmpty(), # routing_id
-                ParseLongEmptyAsZero(), # ls_id
-                ParseNullAsEmpty(), # ospf_area_id
-                ParseNullAsEmpty(), # isis_area_id
-                ParseNullAsEmpty(), # protocol
-                ParseNullAsEmpty(), # as_path
-                ParseLongEmptyAsZero(), # local_pref
-                ParseLongEmptyAsZero(), # med
-                ParseNullAsEmpty(), # nexthop
-                ParseNullAsEmpty(), # mt_id
-                ParseLongEmptyAsZero(), # local_link_id
-                ParseLongEmptyAsZero(), # remote_link_id
-                ParseNullAsEmpty(), # intf_ip
-                ParseNullAsEmpty(), # nei_ip
-                ParseLongEmptyAsZero(), # igp_metric
-                ParseLongEmptyAsZero(), # admin_group
-                ParseNullAsEmpty(), # max_link_bw
-                ParseNullAsEmpty(), # max_resv_bw
-                ParseNullAsEmpty(), # unresv_bw
-                ParseLongEmptyAsZero(), # te_default_metric
-                ParseNullAsEmpty(), # link_protection
-                ParseNullAsEmpty(), # mpls_proto_mask
-                ParseNullAsEmpty(), # srlg
-                ParseNullAsEmpty(), # link_name
-                ParseNullAsEmpty(), # remote_node_hash
-                ParseNullAsEmpty() # local_node_hash
-            ]
-
-        return processors
+        return defaultCellProcessors + versionSpecificProcessors
