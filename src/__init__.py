34335de95a26c97752ef4fc02dbed7b4c9d2598d
Matvey-Kuk <motakuk@gmail.com>
2016-12-14 14:36:03 -0800

    Made it able to install by pip

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-This "__init__.py" file is to create a package from all classes in the directory.
-"""
-from openbmp import *
-
-__all__ = ['BaseAttribute', 'BmpStat', 'Collector', 'LsLink', 'LsNode',
-           'LsPrefix', 'Message', 'Peer', 'Router', 'UnicastPrefix', 'Message', 'MsgBusFields']
